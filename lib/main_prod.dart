12c724ebdaee4dbf287f4adf316362a288551913
Jeff Corcoran <jcorcoran+github@gmail.com>
2018-11-05 11:46:54 -0500

    Tweak flavor appNames

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_conference_app/app_config.dart';
+import 'package:flutter_conference_app/config.dart';
 import 'package:flutter_conference_app/main_widget.dart';
 
 void main() {
   var configuredApp = new AppConfig(
-    appName: 'Flutter Conference',
+    appName: Config.title,
     flavorName: 'production',
     child: new MyApp(),
   );
